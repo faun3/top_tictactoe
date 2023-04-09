b750498f10a8e4e9863b17316d07e33c6295502e
faun3 <antonioculcea@gmail.com>
2023-04-09 16:34:30 +0300

    write basic logic for rendering the board

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,18 @@
-console.log("hello");
+let Gameboard = {
+  boardArr: ["x", "x", "o", "", "o", "", "x", "", "o"],
+};
+
+const PlayerFactory = (name) => {
+  return { name };
+};
+
+const renderBoard = function (boardArr) {
+  let boardWrapper = document.querySelector(".boardWrapper");
+  for (let i = 0; i < boardArr.length; i++) {
+    let squareDiv = document.createElement("div");
+    squareDiv.textContent = boardArr[i];
+    boardWrapper.appendChild(squareDiv);
+  }
+};
+
+renderBoard();
