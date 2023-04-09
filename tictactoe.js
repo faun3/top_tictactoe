122b7ce3779b486ee956b1fd54df7054e8d0d4f9
faun3 <antonioculcea@gmail.com>
2023-04-09 17:25:14 +0300

    work on clicking logic

--- tictactoe.js
+++ tictactoe.js
@@ -1,19 +1,39 @@
 let Gameboard = {
-  boardArr: ["x", "x", "o", "", "o", "", "x", "", "o"],
+  boardArr: ["x", "x", "o", "x", "o", "", "", "", ""],
+  clearBoard: () => {
+    let boardWrapper = document.querySelector(".boardWrapper");
+    for (let i = 0; i < 9; i++) {
+      boardWrapper.removeChild("div");
+    }
+  },
   renderBoard: () => {
     let boardWrapper = document.querySelector(".boardWrapper");
     for (let i = 0; i < 9; i++) {
       let squareDiv = document.createElement("div");
-      let divP = document.createElement("p");
-      divP.textContent = Gameboard.boardArr[i];
-      squareDiv.appendChild(divP);
+      squareDiv.classList.add("square");
+      squareDiv.textContent = Gameboard.boardArr[i];
       boardWrapper.appendChild(squareDiv);
     }
   },
 };
 
-const PlayerFactory = (name) => {
-  return { name };
+const PlayerFactory = (name, symbol) => {
+  const makeMove = () => {
+    let boardSquares = document.querySelectorAll(".square");
+    boardSquares.forEach((square) => {
+      square.addEventListener("click", () => {
+        square.textContent = symbol;
+        //Gameboard.renderBoard();
+      });
+    });
+  };
+  return { name, symbol, makeMove };
 };
 
+player1 = PlayerFactory("Gamer 1", "x");
+player2 = PlayerFactory("Gamer 2", "o");
+
 Gameboard.renderBoard();
+
+player1.makeMove();
+player2.makeMove();
